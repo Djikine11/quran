e234ec22013224bef98d2f15f22cd3c218589a3a
ouz <djikine@gmail.com>
2023-12-17 02:26:51 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuranService } from './quran.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  resp: any
+  constructor(private Hservice: QuranService){}
+  ngOnInit(): void {
+      this.Hservice.connectBack().forEach((res) => {
+        this.resp = res.data.ayahs
+        console.log(this.resp)
+      })
+  }
   title = 'app';
 }
